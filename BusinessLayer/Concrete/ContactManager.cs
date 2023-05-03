85f9fef7ac61257f572e7eda390ef1e388855f07
Feyza Çakmak <feyzacakmakk1@gmail.com>
2023-05-03 23:19:40 +0300

    Hakkımızda ve İletişim sayfası eklendi. Footer kısmında düzenlemeler yapıldı.

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class ContactManager : IContactService
+	{
+		IContactDal _contacDal;
+
+		public ContactManager(IContactDal contacDal)
+		{
+			_contacDal = contacDal;
+		}
+
+		public void ContactAdd(Contact contact)
+		{
+			_contacDal.Insert(contact);
+		}
+	}
+}
---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreDemo.Controllers
+{
+	public class ContactController : Controller
+	{
+		ContactManager contactManager = new ContactManager(new EFContactRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Contact contact)
+		{
+			contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+			contact.ContactStatus = true;
+			contactManager.ContactAdd(contact);
+			return RedirectToAction("Index", "Blog");
+			//işlem bittikten sonra BlogControllerdaki Index Action'ına yönlendir
+		}
+	}
+}
